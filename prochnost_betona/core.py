5a7dabf64e6021538a3844bcf7bbffc25c6fc4a8
RomanDoc <yadonistroman@mail.ru>
2023-11-27 23:13:24 +0700

    patch_2

--- prochnost_betona/core.py
+++ prochnost_betona/core.py
@@ -1,4 +1,4 @@
-from config_data.PROCHNOST import KICK_UP, KICK_DOWN, KICK_SIDE
+from prochnost_betona.utils.CRUD import kick_up, kick_down, kick_side
 
 
 def str_in_list(number_str: str) -> list:
@@ -23,12 +23,25 @@ def average_list(number_list: list) -> float:
     :type number_list: list[int]
     :return: среднее значение
     """
-    return round(sum(number_list) / len(number_list), 1)
+    return int(round(sum(number_list) / len(number_list)))
 
 
+def prochnost(target: int, kick: str) -> None:
+    if kick == 'вверх':
+        print(kick_up(target))
+    elif kick == 'вниз':
+        print(kick_down(target))
+    elif kick == 'вбок':
+        print(kick_side(target))
+    else:
+        print('Ошибка в данных')
+
+
+kick = input('Введит направление удара (вверх, вниз или вбок): ').lower()
 number_str = input('Введите значение через пробел: ')
 number_list = str_in_list(number_str)
-average_number = average_list(number_list)
+average_number = int(average_list(number_list))
+prochnost(average_number, kick)
 
 
-print(average_number)
+# print(type(average_number))
